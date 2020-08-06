7a591876c7b36b229af1b407c9b5e3ebba43fb91
Akhil Raju <akhildraju@gmail.com>
2020-08-05 19:47:59 -0700

    First day assignment

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from item import Item
+from player import Player
 
 # Declare all the rooms
 
@@ -49,3 +51,30 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+name = input("Enter your name\n")
+
+player = Player(name, room["outside"], items=["nickel", "good for flippin"])
+
+while True:
+    # print(
+    #     f"Current Room: {player.room.name}\nCurrent Items {player.items}\nItems in the Room {player.room.items}\n {player.room.description}")
+    print(
+        f"Current Room: {player.room.name}\n{player.room.description}")
+    playerInput = input(
+        "Enter N: Move North\nEnter S: Move South\nEnter E: Move East\nEnter W: Move West\nEnter Q: Enter q to quit\n").lower()
+    print(f"You moved {playerInput}\n")
+    if playerInput == 'n' or playerInput == 's' or playerInput == 'w' or playerInput == 'e' or playerInput == 'q':
+        if playerInput == "n":
+            player.room = player.room.n_to
+        elif playerInput == "s":
+            player.room = player.room.s_to
+        elif playerInput == "w":
+            player.room == player.room.w_to
+        elif playerInput == "e":
+            player.room == player.room.e_to
+        elif playerInput == "q":
+            print("Game Over (For Now)")
+            break
+        else:
+            print("Enter a cardinal direction on your keyboard")
